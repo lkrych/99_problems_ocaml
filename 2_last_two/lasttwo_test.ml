0d3f73e6c92536b73ab3d1b89948c529dcc7b1aa
Leland Krych <Leland.Krych@gmail.com>
2019-03-13 08:17:45 -0700

    update test message for last_two

--- 2_last_two/lasttwo_test.ml
+++ 2_last_two/lasttwo_test.ml
@@ -1,7 +1,7 @@
 open OUnit2;;
 open Lasttwo;;
 
-let tests = "test suite for last" >::: [
+let tests = "test suite for last_two" >::: [
       "empty" >:: (fun _ -> assert_equal None (last_two []));
       "one"   >:: (fun _ -> assert_equal (None) (last_two [1]));
       "many"  >:: (fun _ -> assert_equal (Some ("c", "d")) (last_two [ "a"; "b"; "c"; "d"]));
