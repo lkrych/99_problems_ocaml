25d2c169fbf9e4e6b15eaf10a11991237b76e58d
Leland Krych <Leland.Krych@gmail.com>
2019-03-20 08:09:57 -0700

    in base case add to current acc

--- 9_pack_consec/pack.ml
+++ 9_pack_consec/pack.ml
@@ -1,7 +1,7 @@
 let pack list =
   let rec aux current acc = function
     | [] -> []
-    | [x] -> (x :: current)
+    | [x] -> (x :: current) :: acc
     | a :: (b :: _ as t) ->
       if a = b then aux (a :: current) acc t
       else aux [] ((a::current) :: acc) t in
