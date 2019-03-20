5465762e80b2abb3cc7e941fe8fff1d4f15e8148
Leland Krych <Leland.Krych@gmail.com>
2019-03-20 08:10:09 -0700

    fix syntax in pack_test.ml

--- 9_pack_consec/pack_test.ml
+++ 9_pack_consec/pack_test.ml
@@ -5,7 +5,7 @@ let tests = "test pack should pack consecutive duplicates into sublists" >::: [
   "empty" >:: (fun _ -> assert_equal [] (pack []));
   "no duplicates" >:: (fun _ -> assert_equal [[1]; [2]; [3]; [4]] (pack [1; 2; 3; 4] ));
   "some duplicates" >:: (fun _ -> assert_equal [[1; 1; 1]; [2]; [3]; [4]]  (pack [1; 1; 1; 2; 3; 4]));
-  "multiple duplicates" >:: (fun _ -> assert_equal [[1; 1; 1]; [2]; [3]; [4; 4] [1]] (pack [1; 1; 1; 2; 3; 4; 4; 1]));
+  "multiple duplicates" >:: (fun _ -> assert_equal [[1; 1; 1]; [2]; [3]; [4; 4]; [1]] (pack [1; 1; 1; 2; 3; 4; 4; 1]));
 ]
 
 let _ = run_test_tt_main tests
