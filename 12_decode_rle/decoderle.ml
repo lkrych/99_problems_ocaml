d6a82d1ee0580c1953632d61fdfe171ffe894ffa
Leland Krych <Leland.Krych@gmail.com>
2019-03-29 08:09:08 -0700

    add decoderle exercise

--- 12_decode_rle/decoderle.ml
+++ 12_decode_rle/decoderle.ml
@@ -1,3 +1,14 @@
 type 'a rle =
   | One of 'a
   | Many of int * 'a;;
+
+let decoderle list = 
+  let rec many acc n x =
+    if n = 0 then acc else many (x::acc) (n-1) x in
+  
+  let rec aux acc = function
+  | [] -> acc
+  | One x :: tail -> aux (x::acc) tail
+  | Many (n, x) :: tail -> aux (many acc n x) tail in
+
+  List.rev(aux [] list)
--- 12_decode_rle/decoderle_test.ml
+++ 12_decode_rle/decoderle_test.ml
@@ -1,8 +1,8 @@
-Open OUnit;;
-Open Decoderle;;
+open OUnit;;
+open Decoderle;;
 
 let tests = "test decoderle should decode a run length encoded array into the original array" >::: [
-  "no elements in input" >:: (fun _ -> assert_equal nil (decoderle []));
+  "no elements in input" >:: (fun _ -> assert_equal [] (decoderle []));
   "no repeats" >:: (fun _ -> assert_equal ["a"; "b"; "c"; "d"] (decoderle [ One "a"; One "b"; One "c"; One "d"]));
   "simple input" >:: (fun _ -> assert_equal ["a"; "b"; "c"; "d"; "a"; "a"] ( decoderle [ One "a"; One "b"; One "c"; One "d"; Many (2, "a")]));
   "complex input" >:: (fun _ -> assert_equal ["a";"a";"a";"a";"b";"c";"c";"a";"a";"d";"e";"e";"e";"e"] ( decoderle [ Many (4, "a"); One "b"; Many (2, "c"); Many (2, "a"); One "d"; Many (4, "e")])) ;
