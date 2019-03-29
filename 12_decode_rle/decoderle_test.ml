66da02564fca29ce96c8c30ae2972b27fa438608
Leland Krych <Leland.Krych@gmail.com>
2019-03-29 07:29:21 -0700

    initialize test runner

--- 12_decode_rle/decoderle_test.ml
+++ 12_decode_rle/decoderle_test.ml
@@ -7,3 +7,5 @@ let tests = "test decoderle should decode a run length encoded array into the or
   "simple input" >:: (fun _ -> assert_equal ["a"; "b"; "c"; "d"; "a"; "a"] ( decoderle [ One "a"; One "b"; One "c"; One "d"; Many (2, "a")]));
   "complex input" >:: (fun _ -> assert_equal ["a";"a";"a";"a";"b";"c";"c";"a";"a";"d";"e";"e";"e";"e"] ( decoderle [ Many (4, "a"); One "b"; Many (2, "c"); Many (2, "a"); One "d"; Many (4, "e")])) ;
 ]
+
+let _ = run_test_tt_main tests
