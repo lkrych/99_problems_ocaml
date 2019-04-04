b59c67d524bcb2197a0b0f1ea205f70bfed43966
Leland Krych <Leland.Krych@gmail.com>
2019-04-04 10:10:47 -0500

    make sure splittwo passes the tests

--- 17_split_list_two/splittwo.ml
+++ 17_split_list_two/splittwo.ml
@@ -1,9 +1,9 @@
 let splittwo list index =
   let rec aux i acc = function
-    | [] -> (List.rev acc, [])
-    | x :: tail -> if index = 1
-                then (List.rev x::acc, tail)
-                else aux i+1 (x::acc) tail in
+    | [] -> List.rev acc, []
+    | x :: tail as l -> if i = 0
+                then List.rev acc, l
+                else aux (i-1) (x::acc) tail in
   
-  aux 0 [] list
+  aux index [] list
   
--- 17_split_list_two/splittwo_test.ml
+++ 17_split_list_two/splittwo_test.ml
@@ -2,8 +2,8 @@ open OUnit2;;
 open Splittwo;;
 
 let tests = "Split a list into two parts; the length of the first part is given. " >::: [
-    "empty list" >:: (fun _ -> assert_equal ([],[]) splittwo [] 1 );
-    "simple case" >:: (fun _ -> assert_equal ([1], [2 3]) (splittwo [ 1 2 3] 1));
+    "empty list" >:: (fun _ -> assert_equal ([],[]) (splittwo [] 1 ));
+    "simple case" >:: (fun _ -> assert_equal (["a"], ["b"; "c"]) (splittwo ["a"; "b"; "c"] 1));
     "empty list" >:: (fun _ -> assert_equal (["a"; "b"; "c"], ["d"; "e"; "f"; "g"; "h"; "i"; "j"]) (splittwo ["a";"b";"c";"d";"e";"f";"g";"h";"i";"j"] 3;) );
 
 ]
