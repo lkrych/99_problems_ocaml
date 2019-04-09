a43d09ef586b1302a278a3139dfa4d1352729d91
Leland Krych <Leland.Krych@gmail.com>
2019-04-09 06:55:18 -0700

    Rename range comand in test file

--- 22_range/range_test.ml
+++ 22_range/range_test.ml
@@ -2,9 +2,9 @@ open OUnit2;;
 open Range;;
 
 let tests = "test range functions ability to create a list" >::: [
-    "create a one element list" >:: ( fun _ -> assert_equal [0] (range 0 0));
-    "create a small list" >:: ( fun _ -> assert_equal [1; 2; 3; 4; 5] (range 1 5));
-    "create a reversed small list" >:: ( fun _ -> assert_equal [5; 4; 3; 2; 1] (range 5 1));
+    "create a one element list" >:: ( fun _ -> assert_equal [0] (new_range 0 0));
+    "create a small list" >:: ( fun _ -> assert_equal [1; 2; 3; 4; 5] (new_range 1 5));
+    "create a reversed small list" >:: ( fun _ -> assert_equal [5; 4; 3; 2; 1] (new_range 5 1));
 ]
 
 let _ = run_test_tt_main tests
