88178729f11473e2f031a0a8343d4934b4839560
Leland Krych <Leland.Krych@gmail.com>
2019-04-12 08:24:30 -0700

    finish permutation

--- 25_permutation/permutation.ml
+++ 25_permutation/permutation.ml
@@ -1,31 +1,19 @@
 open Random;;
-open Printf;;
 
-let print_list list = String.concat "" (List.map string_of_int list)
-
-let perm list =
-  let rec sample_wo_replacement list already_selected =
-    let () = Printf.printf "sample is being called with %s and %s" (print_list list) (print_list already_selected) in
-    let length = (List.length list) - 1 in
-    let selected = Random.int(length) in
-    if List.mem selected already_selected
-    then sample_wo_replacement list already_selected
-    else selected 
-  in
-
-    (* find element at index x *)
-  let rec find_index_x x = function
+let rec perm list =
+  let rec extract acc n = function (* use accumulator to store elements you've already seen *)
   | [] -> raise Not_found
-  | h :: t -> if x = 0 then h else find_index_x (x-1) t 
+  | h :: t -> if n = 0 then (h, acc @ t) else extract (h::acc) (n-1) t
   in
 
-  let rec aux acc selected = function
-    | [] -> acc
-    | h :: t as l ->
-      let select = sample_wo_replacement l selected in
-      let old_el = find_index_x select l in
-      let () = Printf.printf "acc is %s, selected is %s, select is %d, old el is %d" (print_list acc) (print_list selected) select old_el in
-      aux (old_el::acc) (select::selected) t 
+  let extract_rand list len =
+    extract [] (Random.int len) list
   in
-  
-  aux [] [] list;;
+
+  let rec aux acc list len =
+    if len = 0 then acc else
+    let picked, rest = extract_rand list len in
+    aux (picked::acc) rest (len - 1)
+  in
+
+  aux [] list (List.length list)
--- 25_permutation/permutation_test.ml
+++ 25_permutation/permutation_test.ml
@@ -5,8 +5,8 @@ let print_list list = String.concat "" (List.map string_of_int list)
 
 let tests = "test code that creates a random combination of a list" >::: [
   "empty list" >:: ( fun _ -> assert_equal [] (perm []) ~printer:print_list);
-  "small list" >:: ( fun _ -> assert_equal [2; 1; 3;] (perm [1; 2; 3]) ~printer:print_list);
-  "bigger list" >:: ( fun _ -> assert_equal [4; 5; 3; 2; 1] (perm [1; 2; 3; 4; 5]) ~printer:print_list);
+  "small list" >:: ( fun _ -> assert_equal [2; 3; 1;] (perm [1; 2; 3]) ~printer:print_list);
+  "bigger list" >:: ( fun _ -> assert_equal [4; 1; 2; 3; 5] (perm [1; 2; 3; 4; 5]) ~printer:print_list);
 ]
 
 let _ = run_test_tt_main tests
