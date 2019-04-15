d22254cdc6e0ec293fd02e234eb72fe043b1e9fb
Leland Krych <Leland.Krych@gmail.com>
2019-04-15 07:37:25 -0700

    update algorithm to be correct

--- 31_is_prime/prime.ml
+++ 31_is_prime/prime.ml
@@ -2,10 +2,9 @@ open Printf;;
 
 let is_prime x =
   let rec aux n =
-    Printf.printf "Checking %d \n" n
     if n > (x/2)
     then true
-    else if n mod 2 = 0
+    else if x mod n = 0
     then false
     else aux (n+1) in
   aux 2
